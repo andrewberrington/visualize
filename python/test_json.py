7af78bc0bd7a002092f7c9893f8902388d5ba73e
Phil Austin <paustin@eos.ubc.ca>
2017-05-09 14:32:33 -0700

    write out executables

--- python/test_json.py
+++ python/test_json.py
@@ -1,9 +1,17 @@
 import json
 import glob
-filelist = glob.glob('cloudtracker/cloudtracker*.nc')
-with open('meta.json', 'w') as f:
-    json.dump(filelist, f, indent=4)
-with open('meta.json', 'r') as f:
-    cloudnames = json.load(f)
 
-print(cloudnames)
+firsttime=False
+if firsttime:
+    empty_dict={'vdfcreate':'vdfcreate_here','raw2vdf':'raw2vdf here'}
+    with open('meta.json', 'w') as f:
+        json.dump(empty_dict, f, indent=4)
+else:
+    with open('meta.json', 'r') as f:
+        the_dict=json.load(f)
+
+filelist=sorted(glob.glob('/media/loh/cloudtracker*nc'))
+the_dict['filenames']=filelist        
+with open('meta.json', 'w') as f:
+    json.dump(the_dict,f, indent=4)
+
