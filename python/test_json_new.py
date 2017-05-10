f6efe9c59f57303cd1e875508d7f035d9c2559b4
Andrew Berrington <remetallized@gmail.com>
2017-05-10 15:11:26 -0700

    forgot to add file in prev commit

---
+++ python/test_json_new.py
@@ -0,0 +1,31 @@
+import json
+import glob
+import argparse
+
+def main():
+    firsttime = False
+    if firsttime:
+        empty_dict = {'vdfcreate': '/Applications/VAPOR/VAPOR.app/Contents/MacOS/vdfcreate',
+                    'raw2vdf': '/Applications/VAPOR/VAPOR.app/Contents/MacOS/raw2vdf'}
+        with open(f'{args.json_name}.json', 'w') as f:
+            json.dump(empty_dict, f, indent=4)
+    else:
+        with open(f'{args.json_name}.json', 'r') as f:
+            the_dict = json.load(f)
+
+    filelist = sorted(glob.glob(f'{args.filedir}/*.nc'))
+    the_dict['filenames'] = filelist
+    with open(f'{args.json_name}.json', 'w') as f:
+        json.dump(the_dict, f, indent=4)
+
+if __name__ == "__main__":
+    linebreaks = argparse.RawTextHelpFormatter
+    descrip = globals()['__doc__']
+    parser = argparse.ArgumentParser(description=descrip,
+                                     formatter_class=linebreaks)
+    parser.add_argument('os', help='operating system (eg. linux, mac)')
+    parser.add_argument('filedir', help='path to directory with netcdf files')
+    parser.add_argument('json_name', help='name of outputted json file')
+    args = parser.parse_args()
+    main()
+    print(args)
