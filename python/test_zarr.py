2024a0e56018fd7d8cc119761e922d3415579ba5
Andrew Berrington <remetallized@gmail.com>
2017-06-20 16:18:20 -0700

    Delete test_zarr.py

--- python/test_zarr.py
+++
@@ -1,8 +0,0 @@
-import zarr
-import numpy as np
-
-zfile = zarr.open_group('/Users/berringtonaca/research/visualize/python/parquet_zarr/bomex_0000019560_zarr', mode='read')
-data = (zfile['QN'][:][0][[3,8],2:4,5:8])
-
-# var_data = the_in[varname][:][0][:, var_y, var_x]
-print(np.shape(data))
